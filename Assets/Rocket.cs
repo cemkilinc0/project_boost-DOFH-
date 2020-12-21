335af550a541f41750d14a5193f200ff31e93523
cemkilinc0 <cemkilinc0@gmail.com>
2020-12-21 23:23:16 +0300

    boosters and rotation
    Just a layout. funcs missing temp with prints.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Boosters activated!!");
+        }
+        if (Input.GetKey(KeyCode.A) && (!Input.GetKey(KeyCode.D)))
+        {
+            print("Rotating LEFT!!");
+        }
+
+        if (Input.GetKey(KeyCode.D) && (!Input.GetKey(KeyCode.A)))
+        {
+            print("Rotating RIGHT!!");
+        }
+
+    }
+}
