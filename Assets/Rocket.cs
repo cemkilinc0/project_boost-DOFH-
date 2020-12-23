0a09e3c3bcec93523957f5ff1a03b1b1a7b2b681
cemkilinc0 <cemkilinc0@gmail.com>
2020-12-23 13:35:34 +0300

    booster + rotation added

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,16 +23,25 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            print("Boosters activated!!");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
-        if (Input.GetKey(KeyCode.A) && (!Input.GetKey(KeyCode.D)))
+        if (Input.GetKey(KeyCode.A) && (!Input.GetKey(KeyCode.D))) // Go right
         {
-            print("Rotating LEFT!!");
+            transform.Rotate(Vector3.forward);
         }
 
-        if (Input.GetKey(KeyCode.D) && (!Input.GetKey(KeyCode.A)))
+        if (Input.GetKey(KeyCode.D) && (!Input.GetKey(KeyCode.A))) // Go left
         {
-            print("Rotating RIGHT!!");
+            transform.Rotate(-Vector3.forward);
+        }
+
+        if (Input.GetKey(KeyCode.W) && (!Input.GetKey(KeyCode.S))) // Go backwards
+        {
+            transform.Rotate(Vector3.right);
+        }
+        if (Input.GetKey(KeyCode.S) && (!Input.GetKey(KeyCode.W))) // Go forward
+        {
+            transform.Rotate(-Vector3.right);
         }
 
     }
