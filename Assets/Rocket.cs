a66a7c66e3de1054ee7e0c05c2f69f8d8d3d946f
cemkilinc0 <cemkilinc0@gmail.com>
2020-12-25 15:50:00 +0300

    audio added to rocket ship

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioData;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioData = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,8 +25,14 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            if(!audioData.isPlaying)
+                audioData.Play();
+
             rigidBody.AddRelativeForce(Vector3.up);
         }
+        else
+            audioData.Stop();
+
         if (Input.GetKey(KeyCode.A) && (!Input.GetKey(KeyCode.D))) // Go right
         {
             transform.Rotate(Vector3.forward);
@@ -33,15 +41,6 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.D) && (!Input.GetKey(KeyCode.A))) // Go left
         {
             transform.Rotate(-Vector3.forward);
-        }
-
-        if (Input.GetKey(KeyCode.W) && (!Input.GetKey(KeyCode.S))) // Go backwards
-        {
-            transform.Rotate(Vector3.right);
-        }
-        if (Input.GetKey(KeyCode.S) && (!Input.GetKey(KeyCode.W))) // Go forward
-        {
-            transform.Rotate(-Vector3.right);
         }
 
     }
