ae75093ff0e0e7c27f88cc2adde532fc0d2c00dc
cemkilinc0 <cemkilinc0@gmail.com>
2020-12-25 19:49:37 +0300

    test platform + adjustments to rocket movement

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -8,6 +8,8 @@ public class Rocket : MonoBehaviour
     Rigidbody rigidBody;
     AudioSource audioData;
 
+    Vector3 rotVector;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,30 +20,40 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
+
+        rotVector = new Vector3(0, 0, 3);
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            if(!audioData.isPlaying)
+            if (!audioData.isPlaying)
                 audioData.Play();
 
             rigidBody.AddRelativeForce(Vector3.up);
         }
         else
             audioData.Stop();
+    }
+
+    private void Rotate()
+    {
+
+        rigidBody.freezeRotation = true; // take manual control of rotation
 
         if (Input.GetKey(KeyCode.A) && (!Input.GetKey(KeyCode.D))) // Go right
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(rotVector);
         }
 
         if (Input.GetKey(KeyCode.D) && (!Input.GetKey(KeyCode.A))) // Go left
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-rotVector);
         }
 
+        rigidBody.freezeRotation = false;
     }
 }
